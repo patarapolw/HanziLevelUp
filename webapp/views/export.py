65e6c1789f6c2c030b0b19d69b3742749b6d28cd
patarapolw <patarapolw@gmail.com>
2018-07-08 13:09:59 +0700

    Disable exporting

--- webapp/views/export.py
+++ webapp/views/export.py
@@ -5,16 +5,16 @@ from HanziLevelUp.item import db_to_csv
 from webapp import app
 
 
-@app.route('/post/export/<export_type>', methods=['POST'])
-def do_export(export_type):
-    if request.method == 'POST':
-        if export_type == 'csv':
-            db_to_csv()
-            return '1'
-
-    return '0'
-
-
-@app.route('/get/export/<filename>')
-def download_export(filename):
-    return send_from_directory('../tmp', filename, as_attachment=True)
+# @app.route('/post/export/<export_type>', methods=['POST'])
+# def do_export(export_type):
+#     if request.method == 'POST':
+#         if export_type == 'csv':
+#             db_to_csv()
+#             return '1'
+#
+#     return '0'
+#
+#
+# @app.route('/get/export/<filename>')
+# def download_export(filename):
+#     return send_from_directory('../tmp', filename, as_attachment=True)
