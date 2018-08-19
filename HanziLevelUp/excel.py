c6b13024978c9a16c9776fe705f3cab9bbe5e680
patarapolw <patarapolw@gmail.com>
2018-08-19 07:46:44 +0700

    Improved Excel exporting

--- HanziLevelUp/excel.py
+++ HanziLevelUp/excel.py
@@ -1,179 +1,57 @@
 import pyexcel_xlsxwx
-import regex
-from collections import OrderedDict
-import namedlist as nl
+import json
 from datetime import datetime
+from collections import OrderedDict
 
-from CJKhyperradicals.sentence import SpoonFed
-from CJKhyperradicals.dict import HanziDict, Cedict
-from HanziLevelUp.hanzi import get_all_hanzi, HanziLevel
-from HanziLevelUp.vocab import get_all_vocab_plus, VocabToSentence
-from webapp.databases import Sentence
+from webapp.databases import Hanzi, Vocab, Sentence
 
 
-HanziRecord = nl.namedlist('HanziRecord', [
-    'Hanzi', 'Pinyin', 'English', 'Heisig', 'Variants', 'Kanji',
-    'Level', 'Note', 'Tags',
-    ('Created', datetime.now(datetime.now().astimezone().tzinfo).isoformat())
-], default='')
+def export_excel():
+    def _dump(table_object):
+        headers = table_object.__table__.columns.keys()
 
-VocabRecord = nl.namedlist('VocabRecord', [
-    'Vocab', 'Simplified', 'Traditional', 'Pinyin', 'English', 'Sentence',
-    'Level', 'Note', 'Tags',
-    'Source',
-    ('Created', datetime.now(datetime.now().astimezone().tzinfo).isoformat())
-], default='')
+        i = headers.index('data')
+        headers.pop(i)
+        all_keys = set()
+        for record in table_object.query:
+            if record.data is not None:
+                all_keys.update(json.loads(record.data).keys())
+        all_keys = list(all_keys)
+        headers[i:i] = all_keys
 
-SentenceRecord = nl.namedlist('SentenceRecord', [
-    'Sentence', 'Pinyin', 'English',
-    'Levels', 'Note', 'Tags',
-    ('Created', datetime.now(datetime.now().astimezone().tzinfo).isoformat())
-], default='')
+        sheet_data = [headers]
+        for record in table_object.query:
+            sheet_data.append(_to_minimal_string(record.entry, all_keys))
 
+        return sheet_data
 
-class ExcelExport:
-    def __init__(self, filename: str):
-        self.filename = filename
-        self.hanzi_formatter = HanziFormatter()
-        self.vocab_formatter = VocabFormatter()
-        self.sentence_formatter = SentenceFormatter()
-
-        try:
-            self.data, self.meta = pyexcel_export.get_data(self.filename)
-
-            self.pre_existing = {
-                'hanzi': set([HanziRecord(*row).Hanzi for row in self.data['Hanzi'][1:]]),
-                'vocab': set(),
-                'sentences': set([SentenceRecord(*row).Sentence for row in self.data['sentences'][1:]]),
-            }
-
-            to_pop = set()
-            for i, row in enumerate(self.data['Vocab'][1:]):
-                record = VocabRecord(*row)
-                if record.Source == 'user':
-                    self.pre_existing['Vocab'].add(record.Entry)
+    def _to_minimal_string(v_list, all_keys):
+        output = []
+        for k, v in v_list.items():
+            if k != 'data':
+                if isinstance(v, datetime):
+                    output.append(v.isoformat())
                 else:
-                    to_pop.add(i + 1)
+                    output.append(v)
+            else:
+                output2 = [None for _ in all_keys]
+                for k2, v2 in json.loads(v).items():
+                    if not isinstance(v2, (str, int)):
+                        output2[all_keys.index(k2)] = json.dumps(v2, ensure_ascii=False)
+                    else:
+                        output2[all_keys.index(k2)] = v2
+                output.extend(output2)
 
-            self.data['Vocab'] = [row for i, row in enumerate(self.data['Vocab']) if i not in to_pop]
+        return output
 
-        except FileNotFoundError:
-            self.data = OrderedDict([
-                ('hanzi', [HanziRecord._fields]),
-                ('vocab', [VocabRecord._fields]),
-                ('sentences', [SentenceRecord._fields])
-            ])
-            self.meta = pyexcel_export.get_meta()
+    data = OrderedDict()
 
-            self.pre_existing = {
-                'hanzi': set(),
-                'vocab': set(),
-                'sentences': set(),
-            }
+    data[Hanzi.__tablename__] = _dump(Hanzi)
+    data[Vocab.__tablename__] = _dump(Vocab)
+    data[Sentence.__tablename__] = _dump(Sentence)
 
-    def from_db(self):
-        ws = self.data['hanzi']
-        for hanzi in get_all_hanzi():
-            if hanzi not in self.pre_existing['Hanzi']:
-                ws.append(self.hanzi_formatter.format(hanzi))
-
-        ws = self.data['vocab']
-        for _, vocab, source in get_all_vocab_plus():
-            if vocab not in self.pre_existing['Vocab']:
-                ws.append(self.vocab_formatter.format(vocab, source))
-
-        ws = self.data['sentences']
-        for sent_query in Sentence.query:
-            sentence = sent_query.sentence
-            if sentence not in self.pre_existing['sentences']:
-                ws.append(self.sentence_formatter.format(sentence))
-
-        self.save()
-
-    def save(self):
-        pyexcel_export.save_data(self.filename, data=self.data, meta=self.meta,
-                                 reset_height=True)
-
-
-class HanziFormatter:
-    def __init__(self):
-        self.hanzi_dict = HanziDict()
-        self.hanzi_level = HanziLevel()
-
-    def format(self, hanzi):
-        print(hanzi)
-
-        entry = self.hanzi_dict.entries.get(hanzi, dict())
-        result = HanziRecord(
-            Hanzi=hanzi,
-            Pinyin=entry.get('Pinyin', ''),
-            English=entry.get('Meaning', ''),
-            Heisig=entry.get('Heisig', ''),
-            Variant=entry.get('Variant', ''),
-            Kanji=entry.get('Kanji', ''),
-            Level=self.hanzi_level.get_hanzi_level(hanzi)
-        )
-
-        return result
-
-
-class VocabFormatter:
-    def __init__(self):
-        self.cedict = Cedict()
-        self.hanzi_level = HanziLevel()
-        self.vocab_to_sentence = VocabToSentence()
-
-    def format(self, entry, source):
-        print(entry, source)
-
-        dict_result = list(self.cedict.search_vocab(entry))
-        simplified = ', '.join([item['simplified'] for item in dict_result])
-        traditional = ', '.join([item['traditional'] for item in dict_result])
-        pinyin = ', '.join([item['pinyin'] for item in dict_result])
-        english = ', '.join([item['english'] for item in dict_result])
-        sentences = self.vocab_to_sentence.convert(entry, online=False)
-        # traditional = pinyin = english = ''
-
-        level = max([self.hanzi_level.get_hanzi_level(hanzi) for hanzi in entry])
-
-        result = VocabRecord(
-            Entry=entry,
-            Simplified=simplified,
-            Traditional=traditional,
-            Pinyin=pinyin,
-            English=english,
-            Sentence='\n'.join(['\n'.join(pair) for pair in sentences]),
-            Level=level,
-            Source=source
-        )
-
-        return result
-
-
-class SentenceFormatter:
-    def __init__(self):
-        self.spoon_fed = SpoonFed()
-        self.hanzi_level = HanziLevel()
-
-    def format(self, sentence):
-        print(sentence)
-
-        lookup = list(self.spoon_fed.get_sentence(sentence))
-        if len(lookup) > 0:
-            english = lookup[0]['english']
-            try:
-                pinyin = lookup[0]['pinyin']
-            except IndexError:
-                pinyin = ''
-        else:
-            english = pinyin = ''
-
-        result = SentenceRecord(
-            Sentence=sentence,
-            Pinyin=pinyin,
-            English=english,
-            Levels=', '.join([str(self.hanzi_level.get_hanzi_level(char)) for char in sentence
-                              if regex.match(r'\p{IsHan}', char)])
-        )
-
-        return result
+    pyexcel_xlsxwx.save_data(
+        'user/HanziLevelUp.xlsx', data,
+        config={'format': None}
+        # config={'format': {'_default': {'num_format': '0'}}}
+    )
--- webapp/views/export.py
+++ webapp/views/export.py
@@ -1,41 +1,14 @@
 from flask import send_from_directory, Response
 
-from collections import OrderedDict
-from datetime import datetime
-import pyexcel_xlsxwx
+from HanziLevelUp.excel import export_excel
 
 from webapp import app
-from webapp.databases import Hanzi, Vocab, Sentence
 
 
 @app.route('/post/export/<export_type>', methods=['POST'])
 def do_export(export_type):
-    def _to_minimal_string(v_list):
-        output = []
-        for v in v_list:
-            if isinstance(v, datetime):
-                output.append(str(v))
-            else:
-                output.append(v)
-
-        return output
-
     if export_type == 'excel':
-        data = OrderedDict()
-
-        data['hanzi'] = [Hanzi.__table__.columns.keys()]
-        for hanzi in Hanzi.query:
-            data['hanzi'].append(_to_minimal_string(hanzi.entry.values()))
-
-        data['vocab'] = [Vocab.__table__.columns.keys()]
-        for vocab in Vocab.query:
-            data['vocab'].append(_to_minimal_string(vocab.entry.values()))
-
-        data['sentences'] = [Sentence.__table__.columns.keys()]
-        for sentence in Sentence.query:
-            data['sentences'].append(_to_minimal_string(sentence.entry.values()))
-
-        pyexcel_xlsxwx.save_data('user/HanziLevelUp.xlsx', data, config={'format': None})
+        export_excel()
         return Response(status=201)
 
     return Response(status=304)
